94c2a2769c0515174c6d94fb23d332ac6e3e1d2d
Eugene K <player766@gmail.com>
2017-04-20 20:45:35 +0200

    edits2

--- app/src/main/java/ru/sberbank/learning/packages/PackagesAdapter.java
+++ app/src/main/java/ru/sberbank/learning/packages/PackagesAdapter.java
@@ -2,14 +2,12 @@ package ru.sberbank.learning.packages;
 
 import android.content.pm.PackageInfo;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
