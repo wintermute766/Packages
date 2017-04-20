52d8663c55aa2100a15371653a959e9d7d2c93e2
Eugene K <player766@gmail.com>
2017-04-20 22:08:34 +0200

    Toast

--- app/src/main/java/ru/sberbank/learning/packages/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/packages/MainActivity.java
@@ -1,15 +1,20 @@
 package ru.sberbank.learning.packages;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
 
     private ListView packagesListView;
+    private PackagesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,24 @@ public class MainActivity extends Activity {
         packagesListView = (ListView) findViewById(R.id.list_packages);
 
         List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
+        adapter = new PackagesAdapter(packages);
         packagesListView.setAdapter(new PackagesAdapter(packages));
+
+        packagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PackageInfo info = adapter.getItem(position);
+                Intent startIntent = getPackageManager()
+                        .getLaunchIntentForPackage(info.packageName);
+
+                if (startIntent != null) {
+                    startActivity(startIntent);
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            getString(R.string.message_cannot_be_launched, info.packageName),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
